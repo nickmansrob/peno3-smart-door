08b89a6f17cbab5106ec1d0734e03d0af21332ea
RvZELD <robin.vandenhoeck@gmail.com>
2022-10-11 17:06:29 +0200

    added euclidean distance

---
+++ server/backend/src/face.ts
@@ -0,0 +1,6 @@
+function euclidDistance(point1: number[], point2: number[]): number {
+  const sum = point1.map((point, index) => {
+    return Math.pow((point - point2[index]), 2)
+  }).reduce((previous, current) => previous + current, 0)
+  return Math.sqrt(sum)
+}
