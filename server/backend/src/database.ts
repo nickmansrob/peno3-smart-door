e1210a4ca2510ca74e3271a720e87dcb0ec44d1d
Rob Nickmans <rob.nickmans@rndevelopment.be>
2022-10-13 20:57:43 +0200

    Trigger ci

--- server/backend/src/database.ts
+++ server/backend/src/database.ts
@@ -1,6 +1,6 @@
+import { mockRecord, mockUser } from '../mocks/user.js'
 import { AuthRecord, Data, User, ValidationError } from './types'
 import { Low, JSONFile } from 'lowdb'
-import { mockRecord, mockUser } from '../mocks/user.js'
 
 export async function initializeDatabase(): Promise<Low<Data>> {
   const adapter = new JSONFile<Data>('./src/assets/db.json')
