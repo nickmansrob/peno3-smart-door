9df9dd42559938ff9052dcb41ba7728c8679d798
RvZELD <robin.vandenhoeck@gmail.com>
2022-10-15 11:32:17 +0200

    very early draft of handle OTP and remarks

--- server/backend/src/provider.ts
+++ server/backend/src/provider.ts
@@ -2,7 +2,9 @@ import express, { Express, Response, Request } from 'express'
 import { getDatabase, validate } from './database.js'
 import { AuthRecord, Data, FaceToken, OutgoingAccess, User } from './types.js'
 import { Low } from 'lowdb'
-import { OutgoingMessage } from 'http'
+import { OutgoingMessage, request } from 'http'
+import { doesNotMatch } from 'assert'
+import { allowedNodeEnvironmentFlags } from 'process'
 
 export async function start(): Promise<void> {
   const app: Express = express()
@@ -37,16 +39,60 @@ function handlePostUser(req: Request, res: Response): void {
   }
 }
 
-function handleOTP(req: Request, res: Response): void {
+/*
+remarks handleOTP 
+  very early draft
+  a lot of parameters are not possible to add to the user to register in the database (still needs to be added in some way that the frontend can give this info when registering user or send this info to another function to do the rest of the process)
+    facetoken (seperate function or registration process for this?), complexity with all the waiting for responses, in this or in other function?
+  a way to stop the process when a person can't put in their pin because of some reason needs to be added? so that it can be stopped and won't run for eternity
+  sending info to fronted as well?
+    */
+
+async function handleOTP(req: Request, res: Response): Promise<void> {
   if (JSON.parse(req.body)) {
     const stream = req.body
     res.status(200).send()
+
+    const newcode = otp.totpURL // wrong, because ? 
+    // post request
+    await req.body
+    const usercode = req.body
+    if (usercode === newcode){
+      const usertoken = otp.secret
+      //add to database
+      res.status(200).send('OK')
+      // somthing to fronted too? 
+    }
+    else{
+      while (usercode !== newcode) { // | onderbreking krijgen 
+        res.status(200).send('NOT OK') }
+      if (usercode === newcode){
+        const usertoken = otp.secret
+        //add to database
+        const newUser = {id: usertoken, firstName: 'John', lastName: 'Doe', faceToken: 'facetoken', tfaToken: 'to do', roles: ['SUPERVISOR'], dateCreated: 'to do'}
+        addEntity('users', newUser)
+        res.status(200).send('OK')
+        // somthing to fronted too? 
+
+      // else programma just stops
+      }
+      else {
+        res.status(400).send()
+      }
+        
+    }
   } else {
     res.status(400).send(req.body)
   }
 }
 
-async function handleFace(req: Request, res: Response): void {
+
+
+// remarks handleFAce
+//  the treshold needs to be set right
+//  info to the dashboard too?
+
+async function handleFace(req: Request, res: Response): Promise<void> {
   if (JSON.parse(req.body)) {
     const request = req.body as FaceToken
     const db = await getDB()
@@ -63,7 +109,7 @@ async function handleFace(req: Request, res: Response): void {
 
     if(minimum <= threshold) {
 
-      // send user of index to dashboard
+      // send user of index to dashboard ? 
 
       res.status(200).send(JSON.stringify(evaluateAccess('GRANTED', userTable[minimum_idx].firstName)))
     }
@@ -77,12 +123,13 @@ async function handleFace(req: Request, res: Response): void {
     res.status(400).send()
   }
   
-  
 }
 
 
+// TO-DO evaluateAcces:
+//  finding right type for date
+
 function evaluateAccess(access: 'GRANTED' | 'DENIED', user: string): OutgoingAccess{
-  const name = user
   const now = new Date()
   const date = now.toLocaleString()
   let outgoing: OutgoingAccess
