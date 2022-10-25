19847c52b90205b0cef65f39679823a4b8a5f96e
RvZELD <robin.vandenhoeck@gmail.com>
2022-10-25 16:56:44 +0200

    Create UserInfo.ts

---
+++ server/backend/src/UserInfo.ts
@@ -0,0 +1,10 @@
+
+export function handleUserRestriction(_req: Request, res: Response): void {
+  const a = 'test'
+
+}
+
+export function handleUserRecords(_req: Request, res: Response): void {
+  const b = 'test'
+
+}
