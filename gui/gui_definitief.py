7981073eff50d551962759c418d4399df2213c2f
Milo122 <miloroggen@gmail.com>
2022-10-28 17:43:11 +0200

    Update gui_definitief.py

--- gui/gui_definitief.py
+++ gui/gui_definitief.py
@@ -69,8 +69,8 @@ class Home(object):
         self.label_2.setFont(font)
         self.label_2.setStyleSheet("color: rgb(255, 255, 255);")
         self.label_2.setObjectName("label_2")
+        self.label_2.setText("Press any key to start ...")
         
-        self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
         
         # Thread:
@@ -88,10 +88,6 @@ class Home(object):
         self.thread.finished.connect(Dialog.close)
         self.thread.start()
 
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label_2.setText(_translate("Dialog", "Press any key to start ..."))
 
     def otp(self):
         self.window = OTP()
@@ -117,14 +113,6 @@ class Home(object):
             self.window = Verified()
         else:
             self.window = Denied()
-    """
-    def employee_id(self, ID):
-        self.id = ID
-        if self.id in "123456":
-    """
-    
-    def open_newWindow(self):
-        self.window = OTP()
 
 class OTP(object):
 
@@ -159,103 +147,27 @@ class OTP(object):
         self.label_2.setGeometry(QtCore.QRect(0, 0, 800, 150))
         self.label_2.setAlignment(QtCore.Qt.AlignCenter)
 
-        self.textlabel1 = QtWidgets.QLabel(Dialog)
-        self.textlabel1.setGeometry(QtCore.QRect(40, 230,100, 100))
-        self.textlabel1.setStyleSheet("color: rgb(85, 170, 127);"
-        "background-color: rgb(85, 170, 127);"
-        "border-top-left-radius :10px;"
-        "border-top-right-radius : 10px; "
-        "border-bottom-left-radius : 10px; "
-        "border-bottom-right-radius : 10px")
-        self.textlabel1.setAlignment(QtCore.Qt.AlignCenter)
-        font = QtGui.QFont()
-        font.setPointSize(32)
-        self.textlabel1.setFont(font)
-        self.textlabel1.setObjectName("textlabel1")
-        self.textlabel1.setText("")
-        textlabel1 = self.textlabel1
-        
-        self.textlabel2 = QtWidgets.QLabel(Dialog)
-        self.textlabel2.setGeometry(QtCore.QRect(160, 230,100, 100))
-        self.textlabel2.setStyleSheet("background-color: rgb(85, 170, 127);"
-        "color: rgb(255, 255, 255);"
-        "border-top-left-radius :10px;"
-        "border-top-right-radius : 10px; "
-        "border-bottom-left-radius : 10px; "
-        "border-bottom-right-radius : 10px")
-        self.textlabel2.setAlignment(QtCore.Qt.AlignCenter)
-        font = QtGui.QFont()
-        font.setPointSize(32)
-        self.textlabel2.setFont(font)
-        self.textlabel2.setObjectName("textlabel2")
-        self.textlabel2.setText("")
+        labelPositions = [(40, 230), (160, 230), (280, 230), (420, 230), (540, 230), (660, 230)]
 
-        self.textlabel3 = QtWidgets.QLabel(Dialog)
-        self.textlabel3.setGeometry(QtCore.QRect(280, 230,100, 100))
-        self.textlabel3.setStyleSheet("background-color: rgb(85, 170, 127);"
-        "color: rgb(255, 255, 255);"
-        "border-top-left-radius :10px;"
-        "border-top-right-radius : 10px; "
-        "border-bottom-left-radius : 10px; "
-        "border-bottom-right-radius : 10px")
-        self.textlabel3.setAlignment(QtCore.Qt.AlignCenter)
-        font = QtGui.QFont()
-        font.setPointSize(32)
-        self.textlabel3.setFont(font)
-        self.textlabel3.setObjectName("textlabel3")
-        self.textlabel3.setText("")
-        
-        self.textlabel4 = QtWidgets.QLabel(Dialog)
-        self.textlabel4.setGeometry(QtCore.QRect(420, 230,100, 100))
-        self.textlabel4.setStyleSheet("background-color: rgb(85, 170, 127);"
-        "color: rgb(255, 255, 255);"
-        "border-top-left-radius :10px;"
-        "border-top-right-radius : 10px; "
-        "border-bottom-left-radius : 10px; "
-        "border-bottom-right-radius : 10px")
-        self.textlabel4.setAlignment(QtCore.Qt.AlignCenter)
-        font = QtGui.QFont()
-        font.setPointSize(32)
-        self.textlabel4.setFont(font)
-        self.textlabel4.setObjectName("textlabel4")
-        self.textlabel4.setText("")
-        
-        self.textlabel5 = QtWidgets.QLabel(Dialog)
-        self.textlabel5.setGeometry(QtCore.QRect(540, 230,100, 100))
-        self.textlabel5.setStyleSheet("background-color: rgb(85, 170, 127);"
-        "color: rgb(255, 255, 255);"
-        "border-top-left-radius :10px;"
-        "border-top-right-radius : 10px; "
-        "border-bottom-left-radius : 10px; "
-        "border-bottom-right-radius : 10px")
-        self.textlabel5.setAlignment(QtCore.Qt.AlignCenter)
-        font = QtGui.QFont()
-        font.setPointSize(32)
-        self.textlabel5.setFont(font)
-        self.textlabel5.setObjectName("textlabel5")
-        self.textlabel5.setText("")
-        
-        self.textlabel6 = QtWidgets.QLabel(Dialog)
-        self.textlabel6.setGeometry(QtCore.QRect(660, 230,100, 100))
-        self.textlabel6.setStyleSheet("background-color: rgb(85, 170, 127);"
-        "color: rgb(255, 255, 255);"
-        "border-top-left-radius :10px;"
-        "border-top-right-radius : 10px; "
-        "border-bottom-left-radius : 10px; "
-        "border-bottom-right-radius : 10px")
-        self.textlabel6.setAlignment(QtCore.Qt.AlignCenter)
-        font = QtGui.QFont()
-        font.setPointSize(32)
-        self.textlabel6.setFont(font)
-        self.textlabel6.setObjectName("textlabel6")
-        self.textlabel6.setText("")
-        
-        self.retranslateUi(Dialog)
+        self.labels = []
+        for i in range(6):
+            textlabel = QtWidgets.QLabel(Dialog)
+            textlabel.setGeometry(QtCore.QRect(labelPositions[i][0], labelPositions[i][1], 100, 100))
+            textlabel.setStyleSheet("color: rgb(85, 170, 127);"
+            "background-color: rgb(85, 170, 127);"
+            "border-top-left-radius :10px;"
+            "border-top-right-radius : 10px; "
+            "border-bottom-left-radius : 10px; "
+            "border-bottom-right-radius : 10px")
+            textlabel.setAlignment(QtCore.Qt.AlignCenter)
+            textlabel.setFont(font)
+            #textlabel.setObjectName("textlabel{}".format(i))
+            textlabel.setText("")
+
+            self.labels.append(textlabel)
+
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
 
     def otp_fill_in(self):
         global users_otp
@@ -264,17 +176,22 @@ class OTP(object):
         global textlabel1
         if users_otp == None:
             users_otp = number
-            _translate = QtCore.QCoreApplication.translate
-            dia.setWindowTitle(_translate("Dialog", "Dialog"))
-            textlabel1.setStyleSheet("border: 5px solid rgb(85, 170, 127);"
+            self.labels[0].setStyleSheet("border: 5px solid rgb(85, 170, 127);"
             "color: rgb(85, 170, 127);"
             "border-top-left-radius :10px;"
             "border-top-right-radius : 10px; "
             "border-bottom-left-radius : 10px; "
             "border-bottom-right-radius : 10px")
-            textlabel1.setText(number)
+            self.labels[0].setText(number)
         else:
             users_otp += number
+            self.labels[len(users_otp)-1].setStyleSheet("border: 5px solid rgb(85, 170, 127);"
+            "color: rgb(85, 170, 127);"
+            "border-top-left-radius :10px;"
+            "border-top-right-radius : 10px; "
+            "border-bottom-left-radius : 10px; "
+            "border-bottom-right-radius : 10px")
+            self.labels[len(users_otp)-1].setText(number)
         if len(users_otp) == 6:
             global ui
             ui.otp_check(users_otp)
@@ -312,13 +229,8 @@ class Verified(object):
         self.textlabel.setText(welcome_string)
         self.textlabel.setObjectName("textlabel")
 
-        self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
         Dialog.show()
-
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
 
 
 class Denied(object):
