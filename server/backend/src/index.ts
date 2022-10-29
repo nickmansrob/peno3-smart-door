1f273e6347415add5425801004a1e4b24f4dcc4f
Rob Nickmans <rob.nickmans@rndevelopment.be>
2022-10-29 14:57:50 +0200

    Fixed index

--- server/backend/src/index.ts
+++ server/backend/src/index.ts
@@ -1,6 +1,3 @@
-import { DateTime, Interval } from 'luxon'
 import { start } from './Provider.js'
 
-//await start()
-
-console.log(JSON.stringify(Interval.fromDateTimes(DateTime.fromISO('2022-10-21T21:28:15+02:00'), DateTime.fromISO('2022-10-28T21:28:15+02:00'))))
+await start()
