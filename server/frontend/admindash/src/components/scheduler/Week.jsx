543bd1073abd50e92c159772cecc1ff1807ef6b5
Kevin Maes <115015689+Kevinmaeso@users.noreply.github.com>
2022-11-04 16:40:49 +0100

    Update Week.jsx

--- server/frontend/admindash/src/components/scheduler/Week.jsx
+++ server/frontend/admindash/src/components/scheduler/Week.jsx
@@ -1,118 +1,31 @@
 import React, {Component} from 'react';
-import {DayPilot, DayPilotCalendar, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
-//import "./CalendarStyles.css";
-
-const styles = {
-  wrap: {
-    display: "flex"
-  },
-  left: {
-    marginRight: "10px"
-  },
-  main: {
-    flexGrow: "1"
-  }
-};
+import {DayPilotCalendar} from "@daypilot/daypilot-lite-react";
 
 class Calendar extends Component {
 
   constructor(props) {
     super(props);
-    this.calendarRef = React.createRef();
+
     this.state = {
-      viewType: "Week",
-      durationBarVisible: false,
-      timeRangeSelectedHandling: "Enabled",
-      onTimeRangeSelected: async args => {
-        const dp = this.calendar;
-        const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
-        dp.clearSelection();
-        if (!modal.result) { return; }
-        dp.events.add({
-          start: args.start,
-          end: args.end,
-          id: DayPilot.guid(),
-          text: modal.result
-        });
-      },
-      eventDeleteHandling: "Update",
-      onEventClick: async args => {
-        const dp = this.calendar;
-        const modal = await DayPilot.Modal.prompt("Update event text:", args.e.text());
-        if (!modal.result) { return; }
-        const e = args.e;
-        e.data.text = modal.result;
-        dp.events.update(e);
-      },
+      viewType: "Resources",
+      startDate: "2022-11-07",
+      columns: [
+        {name: "Monday", id: "R1"},
+        {name: "Tuesday", id: "R2"},
+        {name: "Wednesday", id: "R3"},
+        {name: "Thursday", id: "R4"},
+        {name: "Friday", id: "R5"},
+        {name: "Saturday", id: "R6"},
+        {name: "Sunday", id: "R7"},
+      ]
     };
-  }
-
-  get calendar() {
-    return this.calendarRef.current.control;
-  }
-
-  componentDidMount() {
-
-    const events = [
-      {
-        id: 1,
-        text: "Event 1",
-        start: "2023-03-07T10:30:00",
-        end: "2023-03-07T13:00:00"
-      },
-      {
-        id: 2,
-        text: "Event 2",
-        start: "2023-03-08T09:30:00",
-        end: "2023-03-08T11:30:00",
-        backColor: "#6aa84f"
-      },
-      {
-        id: 3,
-        text: "Event 3",
-        start: "2023-03-08T12:00:00",
-        end: "2023-03-08T15:00:00",
-        backColor: "#f1c232"
-      },
-      {
-        id: 4,
-        text: "Event 4",
-        start: "2023-03-06T11:30:00",
-        end: "2023-03-06T14:30:00",
-        backColor: "#cc4125"
-      },
-    ];
-
-    const startDate = "2023-03-07";
-
-    this.calendar.update({startDate, events});
-
   }
 
   render() {
     return (
-      <div style={styles.wrap}>
-        <div style={styles.left}>
-          <DayPilotNavigator
-            selectMode={"week"}
-            showMonths={3}
-            skipMonths={3}
-            startDate={"2023-03-07"}
-            selectionDay={"2023-03-07"}
-            onTimeRangeSelected={ args => {
-              this.calendar.update({
-                startDate: args.day
-              });
-            }}
-          />
-        </div>
-        <div style={styles.main}>
-          <DayPilotCalendar
-            {...this.state}
-            ref={this.calendarRef}
-          />
-        </div>
-      </div>
+      <DayPilotCalendar
+        {...this.state}
+      />
     );
   }
 }
