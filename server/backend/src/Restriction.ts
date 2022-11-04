7f039bc30688311a945226bf0d6735333535325d
RvZELD <robin.vandenhoeck@gmail.com>
2022-11-04 17:52:39 +0100

    small and not working changes at restriction

--- server/backend/src/Restriction.ts
+++ server/backend/src/Restriction.ts
@@ -1,4 +1,5 @@
-import { DateTime } from 'luxon'
+import { DateTime, Interval } from 'luxon'
+import { exitCode } from 'process'
 import { getDatabase } from './Database.js'
 import { OutgoingAccess, User } from './types.js'
 
@@ -8,24 +9,22 @@ export async function userRestrictions(user: User): Promise<OutgoingAccess> {
   const id = user.id
   const role = user.roles
   const db = await getDatabase()
-  const restrictionsByUser = db.data.restrictions[day].filter((restriction) => restriction[day].users.id === id)
-  const restrictionsByGroup = db.data.restrictions[day].filter((restriction) => restriction[day].group.group === role)
-  const allRestrictions = [...restrictionsByUser, ...restrictionsByGroup]
-  // const intervals = allRestrictions.map(allRestrictions.interval)
-  const check = allRestrictions.map((interval) => interval.contains(time)).filter((x) => x === false)
-  if (check.length === 0) {
-    const Access: OutgoingAccess = {
-      firstName: user.firstName,
-      timestamp: DateTime.now().setZone('Europe/Brussels'),
-      access: 'GRANTED',
-    }
-    return Access
-  } else {
-    const Access: OutgoingAccess = {
-      firstName: user.firstName,
-      timestamp: DateTime.now().setZone('Europe/Brussels'),
-      access: 'DENIED',
-    }
-    return Access
-  }
+  console.log('test log', db.data.restrictions['MON'])
+  const restrictionsByUser = db.data.restrictions['MON'].users.filter(restriction => restriction['MON'].users.id === id)
+  console.log('restriction user', restrictionsByUser)
+  const restrictionsByUsertime = (db.data.restrictions['MON'].users.filter(restriction => restriction['MON'].users.id === id)).map(restriction => restriction.interval) // test if map function would work
+  console.log(restrictionsByUsertime) // byUsertime, gebruiken we niet maar had ik even erbij gezet voor de map te proberen
+  const restrictionsByGroup = db.data.restrictions['MON'].groups.filter(restriction => restriction['MON'].group.group === role)
+  return restrictionsByUsertime
+//   const allRestrictions = [...restrictionsByUser, ...restrictionsByGroup]
+//   // const intervals = allRestrictions.map(allRestrictions.interval)
+//   const check = (allRestrictions.map(interval => interval.contains(time))).filter(x => x === false)
+//   if (check.length===0){
+//     const Access : OutgoingAccess = {firstName : user.firstName, timestamp : DateTime.now().setZone('Europe/Brussels'), access: 'GRANTED'}
+//     return Access
+//   }
+//   else {
+//     const Access : OutgoingAccess = {firstName : user.firstName, timestamp : DateTime.now().setZone('Europe/Brussels'), access: 'DENIED'}
+//     return Access
+//   }
 }
