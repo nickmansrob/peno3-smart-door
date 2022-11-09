9321b46c819e2b2551ebdbb6ab1a3f4136f9b554
RvZELD <115017201+RvZELD@users.noreply.github.com>
2022-11-09 14:31:24 +0100

    Backend/finishing (#17)
    * small and not working changes at restriction
    
    * Fix restrictions
    
    * Function inInterval, userRestrictions furhter
    
    * Testing template (test user and fake restrictions)
    
    * testing and correcting restrictions
    
    Co-authored-by: Rob Nickmans <rob.nickmans@rndevelopment.be>

--- server/backend/src/Restriction.ts
+++ server/backend/src/Restriction.ts
@@ -1,31 +1,91 @@
-import { DateTime } from 'luxon'
+import { DateTime, Interval } from 'luxon'
+import { exitCode } from 'process'
 import { getDatabase } from './Database.js'
-import { OutgoingAccess, User } from './types.js'
+import { CustomInterval, GroupRestriction, OutgoingAccess, Role, User, UserRestriction } from './types.js'
 
-export async function userRestrictions(user: User): Promise<OutgoingAccess> {
-  const day = DateTime.now().weekdayShort.toUpperCase()
-  const time = DateTime.now()
-  const id = user.id
-  const role = user.roles
+
+
+/**
+ * Na een eerste test is er een fout gebleken in de functie intersectArrays, hij kan de lengte of zo niet lezen
+ * ik kon er niets meer aan aanpassen in de sessie zelf. De test user lijkt ok te werken en deze staat in index
+ * er zijn ook meerde logs per deel geplaats die achteraf bijna allemaal weggaan.
+ * 
+ */
+export async function userRestrictions(accessUser: User): Promise<OutgoingAccess> {
   const db = await getDatabase()
-  const restrictionsByUser = db.data.restrictions[day].filter((restriction) => restriction[day].users.id === id)
-  const restrictionsByGroup = db.data.restrictions[day].filter((restriction) => restriction[day].group.group === role)
-  const allRestrictions = [...restrictionsByUser, ...restrictionsByGroup]
-  // const intervals = allRestrictions.map(allRestrictions.interval)
-  const check = allRestrictions.map((interval) => interval.contains(time)).filter((x) => x === false)
-  if (check.length === 0) {
-    const Access: OutgoingAccess = {
-      firstName: user.firstName,
-      timestamp: DateTime.now().setZone('Europe/Brussels'),
-      access: 'GRANTED',
-    }
-    return Access
-  } else {
-    const Access: OutgoingAccess = {
-      firstName: user.firstName,
-      timestamp: DateTime.now().setZone('Europe/Brussels'),
-      access: 'DENIED',
-    }
+  
+  const currentTime = (DateTime.now().hour * 100 + DateTime.now().minute).toString()
+  const currentDay = DateTime.now().weekdayShort.toUpperCase()
+  const userRestrictions = db.data.restrictions[currentDay].users.filter(
+    (restriction: UserRestriction) => restriction.id === accessUser.id,
+  )?.map( (entry: UserRestriction) => {return entry.interval})  // Array of Costum intervals
+
+  console.log(userRestrictions) // test
+
+  const groupRestrictions = db.data.restrictions[currentDay].groups.filter((restriction: GroupRestriction) =>
+    intersectingArrays(restriction.role, accessUser.roles),
+  )?.map( (entry: GroupRestriction) => {return entry.interval})  // Array of Costum intervals
+
+  console.log(groupRestrictions) // test
+
+  const Restrictions = [...userRestrictions, ...groupRestrictions]  // Array of Costum intervals
+
+  console.log(Restrictions) // test
+  console.log(Restrictions.length) // test
+
+  if (Restrictions.length ===0){
+    const Access : OutgoingAccess = {firstName : accessUser.firstName, timestamp : DateTime.now().setZone('Europe/Brussels'), access: 'GRANTED'}
+
+    console.log(Access) // test
+
     return Access
   }
+  const booleanRestrictions = Restrictions.map(x => inInterval(currentTime, x))  // Array of booleans
+  if (booleanRestrictions.includes(false)){
+    const Access : OutgoingAccess = {firstName : accessUser.firstName, timestamp : DateTime.now().setZone('Europe/Brussels'), access: 'DENIED'}
+
+    console.log(Access) // test
+
+    return Access
+  }
+  else {
+    const Access : OutgoingAccess = {firstName : accessUser.firstName, timestamp : DateTime.now().setZone('Europe/Brussels'), access: 'GRANTED'}
+
+    console.log(Access) // test
+
+    return Access
+  }
+
+  // OK?: Check if in interval but not with contains as this includes the date and we only want the hour.
+  // OK?: Check for empty restriction, allow if empty
 }
+
+
+function inInterval(currentTime: string, restrictionInterval: CustomInterval): boolean {
+  const currentTimeNumber: string = currentTime.replace(':', '')
+  const minimumEntry : string= restrictionInterval.s.replace(':', '')
+  const maxEntry : string= restrictionInterval.e.replace(':', '')
+  if (currentTimeNumber >= minimumEntry && currentTime <= maxEntry){
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+
+
+/*
+@returns true if arrays intersect, false if not
+*/
+ 
+function intersectingArrays(arr1: Role[], arr2: Role[]): boolean {
+
+  console.log(arr1) // test
+  console.log(arr2) // test
+
+  const biggestArray = arr1.length > arr2.length ? arr1 : arr2
+  const smallestArray = arr1.length <= arr2.length ? arr1 : arr2
+  return biggestArray.filter(value => smallestArray.includes(value)).length !== 0
+}
+
