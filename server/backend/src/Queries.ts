952984e8903a249dd6cddd45e2b88d804fe9e1cc
RvZELD <robin.vandenhoeck@gmail.com>
2022-11-15 16:01:22 +0100

    getRangeEntries and testing db update

--- server/backend/src/Queries.ts
+++ server/backend/src/Queries.ts
@@ -1,7 +1,8 @@
 import { getDatabase } from './Database.js'
 import { AuthRecord, CustomInterval, UserRecord } from './types.js'
-import { DateTime} from 'luxon'
-import { inInterval } from './Restriction.js'
+import { DateTime, Interval} from 'luxon'
+import { inInterval, checkEnter, checkDate} from './CheckFunctions.js'
+import { start } from 'repl'
 
 export async function stateUser(id: string): Promise<string> {
   const db = await getDatabase()
@@ -48,44 +49,7 @@ function checkDayEnter(record: UserRecord): boolean{
   }
 }
 
-//check on state
-function checkEnter(record: UserRecord): boolean{
-  const state = record.state
-  if (state === 'ENTER'){
-    return true
-  }
-  else{
-    return false
-  }
-}
-// check on interval
 
-export function inInterval(currentTime: string, restrictionInterval: CustomInterval): boolean {
-  const currentTimeNumber: string = currentTime.replace(':', '')
-  const minimumEntry: string = restrictionInterval.s.replace(':', '')
-  const maxEntry: string = restrictionInterval.e.replace(':', '')
-  if (currentTimeNumber >= minimumEntry && currentTime <= maxEntry) {
-    return true
-  } else {
-    return false
-  }
-}
-function checkInterval(record: UserRecord, type: 'DAY'| 'DATE' | 'TIME', day?: string, restrictionInterval?: CustomInterval): boolean{
-  const dateRecord = record.timestamp
-  if (type ==='DATE'){
-    const minimumDate = restrictionInterval.s
-    const maxDate = restrictionInterval.e
-  }
-  if (type === 'TIME'){
-    const currentTime = (DateTime.now().hour * 100 + DateTime.now().minute).toString()
-    const currentTimeNumber: string = currentTime.replace(':', '')
-    const timeRecord = (DateTime.fromISO(dateRecord).hour * 100 + DateTime.fromISO(dateRecord).minute).toString()
-  }
-  if (type === 'DAY'){
-    const today = DateTime.now().weekday
-    const dayRecord = DateTime.fromISO(dateRecord).weekday
-  }
-}
 
 // Amount of employees inside
 // TO-DO ook nog totaal aantal mensen binnen toevoegen
@@ -111,7 +75,7 @@ export async function getEntries(): Promise<number>{ // Amount current inside
 
 //get range entries : 
 /**
- * frontend geeft req.body.s en e ,  s en e zijn datums (stringvom) dateTime.fromISO(e)....
+ * frontend geeft req.body.s en e ,  s en e zijn datums (stringvom) dateTime.fromISO(e).... wordt list?
  * backend geeft terug: 
  * @return [12, 45, ... 47]
  * doen via (type locaal maken):
@@ -120,4 +84,50 @@ export async function getEntries(): Promise<number>{ // Amount current inside
   const body = req.body as { s: string, e: string }
 }
  */
+export async function getRangeEntries(range: string[]) {
+  // get Database
+  const db = await getDatabase()
+  const records = db.chain.get('records').value() as AuthRecord
+  const idArray = Object.keys(records)
 
+  // Define interval
+  const startTime = DateTime.fromISO(range[0])
+  const endTime = DateTime.fromISO(range[1])
+  const interval = Interval.fromDateTimes(startTime, endTime)
+
+  // console.log('interval', interval) //test
+
+  // const lengthInterval = interval.length('days')
+
+  //dayarray
+  let currentDay = startTime
+  const dayArray = [startTime]
+
+  while (interval.contains(currentDay)){
+    currentDay = currentDay.plus({ days: 1 })
+    dayArray.push(currentDay) 
+    
+    // console.log('tijdens dayArray', dayArray) //test
+  }
+
+  // console.log('einde van dayArray', dayArray) //test
+
+  const allAmountOfEntries = []
+  // for loop to get information out of records
+  for (const date of dayArray){
+ 
+    const inRange = (idArray.map(id => records[id].records).map(userRecords => userRecords.filter(record => checkDate(record, date)))).filter(array => array.length >0)
+
+    // console.log('tussentijdse in range', inRange) //test
+
+    const amountOfEntries = inRange.length
+
+    // console.log('tussentijdse amount of Entries', amountOfEntries) //test
+
+    allAmountOfEntries.push(amountOfEntries)
+
+    // console.log('tussentijdse all amount of entries', allAmountOfEntries) //test
+  }
+
+  return allAmountOfEntries
+}
