ffc8c11fe0add5c247f0cff6cf590374a7f6ed3d
Kevin Maes <115015689+Kevinmaeso@users.noreply.github.com>
2022-11-15 16:00:20 +0100

    navbar

--- server/frontend/admindash/src/components/navbar/Navbar.jsx
+++ server/frontend/admindash/src/components/navbar/Navbar.jsx
@@ -1,20 +1,18 @@
 import "./navbar.scss";
 import ManageSearchIcon from "@mui/icons-material/ManageSearch";
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
         <div className="search">
-          <input type="text" placeholder="Search..." />
-          <ManageSearchIcon></ManageSearchIcon>
+          
+          
         </div>
         <div className="items">
           <div className="item">
-            <img
-              src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-2379004.jpg&fm=jpg&_gl=1*upmnj0*_ga*OTQwNTQwMTM5LjE2NjY3MzQzOTU.*_ga_8JE65Q40S6*MTY2NjczNDM5NS4xLjEuMTY2NjczNDQwMi4wLjAuMA.."
-              className="avatar"
-            />
+             <SupervisorAccountIcon></SupervisorAccountIcon>
           </div>
         </div>
       </div>
@@ -23,3 +21,9 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+/*<div className="search">
+          <input type="text" placeholder="Search..." />
+          <ManageSearchIcon></ManageSearchIcon>
+        </div>*/
