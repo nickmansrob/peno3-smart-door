0822a5f8033c4d61e96085033bbd421c942b53cd
RvZELD <robin.vandenhoeck@gmail.com>
2022-11-15 21:43:39 +0100

    testing files (database expanded, file)

--- server/backend/src/index.ts
+++ server/backend/src/index.ts
@@ -1,9 +1,4 @@
 import { start } from './Provider.js'
-import { latestEntries } from './Queries.js'
 
 //await start()
-
-console.log(await latestEntries(2))
-console.log(await latestEntries(3))
-console.log(await latestEntries(8))
 
