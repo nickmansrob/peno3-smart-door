5e5703a6c3152d1c643b6206f7fb6bd26e70f249
RvZELD <robin.vandenhoeck@gmail.com>
2022-11-18 17:54:56 +0100

    function create record

--- server/backend/src/record.ts
+++ server/backend/src/record.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from 'express'
 import { prisma } from './database.js'
+import { UserRecord } from './types.js'
 
 export async function handleRecordView(req: Request, res: Response): Promise<void> {
   res.json(await getRecords(parseInt(req.query.id as string)))
@@ -22,21 +23,49 @@ async function getRecords(id?: number) {
   }
 }
 
-export async function handleNewRecord(req: Request, res: Response): Promise<void> {
-  // TODO: implement
-}
-
-export async function handleEditRecord(req: Request, res: Response): Promise<void> {
-  // TODO: implement
-}
-
 /**
  *
  * @param userId The id of the user
  * @param method The way the user got in
  * @returns The success state of the creation
  */
-export function createRecord(userId: number, method: string): boolean {
+export async function createRecord(userId: number, method: 'TFA'| 'FACE'): Promise<boolean> {
   // TODO: implement
-  return true
+  const latestUserRecords = getLatestUserRecords() as UserRecord[]
+  if (latestUserRecords){
+    const lastState = latestUserRecords.filter(record => record.id === userId)[0].state
+    try{
+      await prisma.record.create({
+        data:{
+          userId: userId,
+          method: method,
+          state: lastState
+        }
+      })
+    }
+    catch(e) {
+      console.error(e)
+      return false
+    } 
+    return true
+  }
+  else {
+    const laststate = 'ENTER'
+    try{
+      await prisma.record.create({
+        data:{
+          userId: userId,
+          method: method,
+          state: laststate
+        }
+      })
+    }
+    catch(e) {
+      console.error(e)
+      return false
+    } 
+    return true
+  }
+
 }
+
